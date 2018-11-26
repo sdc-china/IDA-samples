7472264bdfaea3f694536c69452a6362b5d7bf45
Li Ming <lmming@cn.ibm.com>
2018-11-26 17:28:35 +0800

    a working demo written with jQuery

--- testing/Headless sample/webapp/html/js/sample.js
+++ testing/Headless sample/webapp/html/js/sample.js
@@ -1,64 +1,247 @@
-// define function
-// attach event
-// run initial code?
+(function () {
+    const server = '9.30.160.68'
+    const port = 9444
+    const requestor = 'requestor'
+    const reviewer = 'reviewer'
+    // let root = 'https://9.30.160.68:9444/'
+    let root = '/'
 
-// let apis = {
-//     login(username){},
-//     claimTask(taskId){},
-//     newRequest(){},
-//     getTasks(){
-//     },
-//     getData(taskId){},
-//     completeTask(taskId, params){}
-// },
-(function(){
-    let loginAs = function(username){
-        $('#current-user').text(username)
+    let showLoading = function () {
         $('#loading-div').show()
-        if(username === 'requestor'){
-        $('#new-request-button').show()
-    }else{
-        $('#new-request-button').hide()
+        $('#main').hide()
     }
-        // show loading
-        // login user with api
-        // update the task list
-        // update available menu
-        // end loading
+    let hideLoading = function () {
         $('#loading-div').hide()
+        $('#main').show()
     }
-    let newRequest = function(){
+    let loginAs = function (username) {
+        currentUser = username
         // show loading
+        showLoading()
+        // update available menu
+        $('#current-user').text(username)
+        if (username === 'requestor') {
+            $('#new-request-button').show()
+        } else {
+            $('#new-request-button').hide()
+        }
+        // login user with api
+        $.post(`${root}ProcessPortal/j_security_check?j_username=${username}&j_password=123456`).done(() => {
+            // update the task list
+            loadTasks()
+        })
+    }
+    let newTaskRow = function (task) {
+        let result = $('<tr></tr>')
+        result.append(`<td>${task['TASK.TKIID']}</td>`)
+        result.append(`<td>${task.TAD_DISPLAY_NAME}</td>`)
+        result.append(`<td>${task.PI_NAME}</td>`)
+        result.append(`<td>${task.DUE}</td>`)
+        let bpmBtn = $('<button type="button" class="btn btn-link">BPM UI</button>')
+        bpmBtn.on('click', {
+            task
+        }, function (event) {
+            showInIframe(event.data.task)
+        })
+        let customBtn = $('<button type="button" class="btn btn-link">Custom UI</button>')
+        customBtn.on('click', {
+            task
+        }, function (event) {
+            showInCustom(event.data.task)
+        })
+        $('<td></td>').append(bpmBtn, customBtn).appendTo(result)
+        return result
+    }
+    let loadTasks = function () {
+        $.ajax({
+            url: `${root}rest/bpm/wle/v1/tasks`,
+            type: 'PUT',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                organization: "byTask",
+                shared: false,
+                teams: [],
+                sort: [{
+                    field: "taskDueDate",
+                    order: "ASC"
+                }],
+                conditions: [{
+                    field: "instanceProcessApp",
+                    operator: "Equals",
+                    value: "HEADLES"
+                }],
+                fields: [
+                    "taskSubject",
+                    "instanceName",
+                    "taskStatus",
+                    "taskDueDate",
+                    "assignedToUser"
+                ],
+                aliases: [],
+                interaction: "claimed_and_available",
+                size: 25
+            })
+        }).done(data => {
+            let tbody = $('#task-table > table > tbody')
+            tbody.empty()
+            if (data.data.items && data.data.items.length > 0) {
+                data.data.items.forEach(task => {
+                    tbody.append(newTaskRow(task))
+                });
+                $('.badge').text(data.data.items.length)
+            } else {
+                $('.badge').text(0)
+            }
+            $('#task-table').show()
+            $('#bpm-ui').hide()
+            $('#custom-ui').hide()
+            // end loading
+            hideLoading()
+        })
+    }
+    let newRequest = function () {
+        // show loading
+        showLoading()
         // submit new request
-        // show task list
-        // end load
+        $.post(`${root}rest/bpm/wle/v1/process?action=start&bpdId=25.ec1b8876-0056-4c4b-a8e0-4115831c3ba2&branchId=2063.c1225bd2-171c-4347-bdb6-33264dd43c1f&parts=header%7CexcludeTaskData%7CexcludeDocs`).done((data) => {
+            console.log(data)
+            // show task list
+            loadTasks()
+        })
     }
-    let showInIframe = function(taskId){
+    let showInIframe = function (task) {
         // hide task list
+        $('#task-table').hide()
         // show iframe div
+        $('#bpm-ui').show()
+        $('#bpm-ui > iframe').attr('src', `${root}teamworks/process.lsw?zWorkflowState=1&zTaskId=${task['TASK.TKIID']}&zResetContext=true`)
     }
-    let showInCustom = function(taskId){
-        // hide task list
+    let showInCustom = function (task) {
+        // hide task lis
+        $('#task-table').hide()
         // show custom ui
+        $('#custom-ui').show()
+        if (task.TAD_DISPLAY_NAME === 'Step: Order Request') {
+            customuiForRequestor(task)
+        } else {
+            customuiForReviewer(task)
+        }
     }
-    let showTasks = function(){
-        // show loading div
-        // empty exiting task table
-        // reload task table
-        // hide loading div
+    let customuiForReviewer = function (task) {
+        let taskId = task['TASK.TKIID']
+        $('#reject').data('taskId', taskId)
+        $('#approve').data('taskId', taskId)
+        $(`#for-${requestor}`).hide()
+        if (!task.OWNER) {
+            $.ajax({
+                url: `${root}rest/bpm/wle/v1/task?action=claim&taskIDs=${taskId}`,
+                type: 'PUT'
+            }).done(() => {
+                updateForReviewer(taskId)
+            })
+        } else {
+            updateForReviewer(taskId)
+        }
+    }
+    let updateForReviewer = function (taskId) {
+        $.get(`${root}rest/bpm/wle/v1/task/${
+    taskId
+  }?action=getData&fields=amount%2Ctype`).done(data => {
+            $('#for-reviewer > div:nth-child(1) > p').text(data.data.resultMap.type)
+            $('#for-reviewer > div:nth-child(2) > p').text(data.data.resultMap.amount)
+            $('#comments').val('')
+            $(`#for-${reviewer}`).show()
+        })
+    }
+    let customuiForRequestor = function (task) {
+        $(`#for-${reviewer}`).hide()
+        let taskId = task['TASK.TKIID']
+        $('#submit').data('taskId', taskId)
+        $.get(`${root}rest/bpm/wle/v1/task/${
+    taskId
+  }?action=getData&fields=amount%2Ctype%2Cmessage`).done(data => {
+            if (data.data.resultMap.type) {
+                $('#request-type').val(data.data.resultMap.type)
+            }
+            $('#amount').val(data.data.resultMap.amount)
+            $('#for-requestor > div:nth-child(3) > p').text(data.data.resultMap.message)
+            $(`#for-${requestor}`).show()
+        })
+    }
+    let submitRequest = function () {
+        let taskId = $('#submit').data('taskId')
+        let type = $('#request-type').val()
+        let amount = $('#amount').val()
+        $.ajax({
+            url: `${root}rest/bpm/wle/v1/task/${taskId}/?action=finish&parts=all&params=${encodeURI(
+                JSON.stringify({
+                    amount,
+                    type
+                })
+              )}`,
+            type: 'PUT'
+        }).done(() => {
+            showTasks()
+        })
+    }
+    let reject = function () {
+        let taskId = $('#reject').data('taskId')
+        let comment = $('#comments').val()
+        let approved = false
+        $.ajax({
+            url: `${root}rest/bpm/wle/v1/task/${taskId}/?action=finish&parts=all&params=${encodeURI(
+                JSON.stringify({
+                    comment,
+                    approved
+                })
+              )}`,
+            type: 'PUT'
+        }).done(() => {
+            showTasks()
+        })
+    }
+    let approve = function () {
+        let comment = $('#comments').val()
+        let approved = true
+        let taskId = $('#approve').data('taskId')
+        $.ajax({
+            url: `${root}rest/bpm/wle/v1/task/${taskId}/?action=finish&parts=all&params=${encodeURI(
+                JSON.stringify({
+                    comment,
+                    approved
+                })
+              )}`,
+            type: 'PUT'
+        }).done(() => {
+            showTasks()
+        })
+    }
+    let showTasks = function () {
+        showLoading()
+        loadTasks()
     }
 
     // binding event
-    $('#requestor-menu').on('click',function(){
-        loginAs('requestor')
+    $('#requestor-menu').on('click', function () {
+        loginAs(requestor)
     })
-    $('#reviewer-menu').on('click',function(){
-        loginAs('reviewer')
+    $('#reviewer-menu').on('click', function () {
+        loginAs(reviewer)
     })
-    $('#new-request-button').on('click', function(){
+    $('#new-request-button').on('click', function () {
         newRequest()
     })
-    $('#inbox-button').on('click', function(){
+    $('#inbox-button').on('click', function () {
         showTasks()
+    })
+    $('#submit').on('click', function () {
+        submitRequest()
+    })
+    $('#reject').on('click', function () {
+        reject()
+    })
+    $('#approve').on('click', function () {
+        approve()
     })
 })()
